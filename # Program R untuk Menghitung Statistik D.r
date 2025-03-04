13a4551045bd2834c86f9a5bddc126b8aa1dbe16
knzvz <itskindaforzey@gmail.com>
2025-03-04 23:51:05 +0700

    Add files via upload

---
+++ # Program R untuk Menghitung Statistik D.r
@@ -0,0 +1,31 @@
+#Perkenalan Diri
+nama<-"YOSUA_RADITYO_PRADANA"
+umur_2025<-19
+universitas<-"SEKOLAH_TINGGI_MANAJEMEN_ASURANSI_TRISAKTI"
+jurusan<-"AKTUARIA"
+semester<-2
+mata_kuliah<-"Dasar_Pemrograman"
+
+#Menampilkan Umur
+umur_2025
+print(umur_2025)
+#Menampilkan Nama
+nama
+print(nama)
+
+#Menghitung Perkalian Sederhana
+a<-5
+5->a
+a
+## [1] 5
+y<-2*a
+2*a->y 
+y 
+## [1] 10
+
+#nilai a sebelumnya
+a
+## [1] 5
+# Nilai a yang baru
+a<- a+7
+a
